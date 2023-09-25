ed6d6cf575147f80d12cf1a406774f749cdf9bef
Rohan Daddekar <rohandaddekar9@gmail.com>
2023-09-25 11:50:26 +0530

    commented testimonial and help section in home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,6 @@
 import Hero from "./partials/Hero/Hero";
 import HowItWorks from "./partials/HowItWorks/HowItWorks";
-import Testimonials from "./partials/Testimonials/Testimonials";
 import TopPerformers from "./partials/TopPerformers/TopPerformers";
-import Help from "./partials/Help/Help";
 
 const Home = () => {
   return (
@@ -10,8 +8,8 @@ const Home = () => {
       <Hero />
       <HowItWorks />
       <TopPerformers />
-      <Testimonials />
-      <Help />
+      {/* <Testimonials /> */}
+      {/* <Help /> */}
     </>
   );
 };
