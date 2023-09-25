c71791deb779f241e54ebb63f6fbc8416a805b3a
Rohan Daddekar <rohandaddekar9@gmail.com>
2023-09-25 12:23:45 +0530

    landing page updated

--- src/pages/Home/partials/Hero/Hero.jsx
+++ src/pages/Home/partials/Hero/Hero.jsx
@@ -8,7 +8,8 @@ import {
 } from "react-icons/bi";
 import { NavLink } from "react-router-dom";
 import { fadeInLeft } from "../../../../data/framerMotionHelper";
-import PrimaryButton from "../../../../components/Buttons/PrimaryButton";
+import StockMarket from "../../../../lottie/stockMarket.json";
+import Lottie from "lottie-react";
 
 const Hero = () => {
   const socialMediaLinks = [
@@ -36,8 +37,8 @@ const Hero = () => {
 
   return (
     <>
-      <div className="bg-[url('../../../images/home/chart-bg-2.png')] bg-cover bg-center min-h-screen pt-32 sm:pt-40 lg:pt-0 overflow-hidden">
-        <div className="min-h-screen mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 h-full flex flex-col justify-center relative">
+      <div className="mt-28 overflow-hidden">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 h-full flex flex-col justify-center relative">
           <div className="grid grid-cols-12 items-center">
             <motion.div
               className="col-span-12 lg:col-span-6"
@@ -45,20 +46,18 @@ const Hero = () => {
               initial="hidden"
               whileInView="visible"
             >
-              <h1 className="text-5xl md:text-6xl font-bold leading-[4rem] md:leading-[5rem] lg:leading-[6rem] text-gray-800">
-                <span className=" text-green-500">Buying</span> and{" "}
+              <h1 className="text-5xl md:text-6xl font-extrabold leading-[4rem] md:leading-[5rem] lg:leading-[6rem] text-gray-800">
+                <span className=" text-green-500">Buying</span> &{" "}
                 <span className=" text-red-500">Selling</span> <br /> Calls from
                 Expert
               </h1>
-              <p className="text-xl md:text-xl text-gray-500 mt-3">
-                Do your research before investment{" "}
-              </p>
-              <PrimaryButton
+
+              {/* <PrimaryButton
                 to="/main"
                 classes="text-white bg-c-green px-8 py-3 font-medium mt-7"
                 rounded="lg"
                 title="Start Investing"
-              />
+              /> */}
 
               <div className="mt-8">
                 <p className="text-md font-semibold text-gray-500">Follow Us</p>
@@ -77,27 +76,11 @@ const Hero = () => {
               </div>
             </motion.div>
             <div className="col-span-12 lg:col-span-6">
-              <img
-                src="../../../images/home/hero-img.png"
-                alt="Hero Image"
-                className=" animate-left-right"
+              <Lottie
+                animationData={StockMarket}
+                className="animate-left-right"
               />
             </div>
-          </div>
-
-          <div className=" absolute top-[6rem]">
-            <img
-              src="../../../images/home/coin.png"
-              alt="coin"
-              className="animate-spin-slow"
-            />
-          </div>
-          <div className=" absolute bottom-24 left-2/4">
-            <img
-              src="../../../images/home/rectangle.png"
-              alt="rectangle"
-              className="animate-spin-slow"
-            />
           </div>
         </div>
       </div>
