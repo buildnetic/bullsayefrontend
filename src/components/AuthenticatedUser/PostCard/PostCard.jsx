5dec69769a778bcbb4da45d76ee22ac4e2c94750
Rohan Daddekar <rohandaddekar9@gmail.com>
2023-11-20 11:18:35 +0530

    updated post card edit, delete ui

--- src/components/AuthenticatedUser/PostCard/PostCard.jsx
+++ src/components/AuthenticatedUser/PostCard/PostCard.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import moment from "moment";
 import { AiFillDelete, AiOutlineLike } from "react-icons/ai";
+import { SlOptions } from "react-icons/sl";
 import { BiCommentDetail, BiShareAlt, BiSolidEditAlt } from "react-icons/bi";
 import { NavLink, useNavigate } from "react-router-dom";
 import ProfileImg from "../../../assets/images/profile-icon.png";
@@ -11,6 +12,12 @@ import axiosInstance from "../../../axiosInstance";
 import { useMutation, useQueryClient } from "react-query";
 import { ToastError, ToastSuccess } from "../../../ToastNotification";
 import SocialShareModal from "../../Modals/SocialShareModal";
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+
+// function classNames(...classes) {
+//   return classes.filter(Boolean).join(" ");
+// }
 
 const PostCard = ({ data, handleCommentClick, classes }) => {
   const { loggedUser } = useSelector((state) => state.user);
@@ -110,18 +117,49 @@ const PostCard = ({ data, handleCommentClick, classes }) => {
                 {data?.user?.name}
               </NavLink>
               {data?.user_id === loggedUser.id && (
-                <div className="flex flex-row items-center gap-2">
-                  <BiSolidEditAlt
-                    className=" cursor-pointer text-lg text-gray-500 hover:text-green-500 transition-all"
-                    title="Edit Post"
-                    onClick={() => navigate(`/post/edit/${data.id}`)}
-                  />
-                  <AiFillDelete
-                    className=" cursor-pointer text-lg text-gray-500 hover:text-red-500 transition-all"
-                    title="Delete Post"
-                    onClick={() => deleteModalFn(data.id)}
-                  />
-                </div>
+                <Menu as="div" className="relative inline-block text-left">
+                  <div>
+                    <Menu.Button className="w-full text-sm">
+                      <SlOptions
+                        className="-mr-1 h-5 w-5 text-gray-400 hover:text-c-green"
+                        aria-hidden="true"
+                      />
+                    </Menu.Button>
+                  </div>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-36 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <div className="py-1">
+                        <Menu.Item>
+                          <div
+                            className="flex justify-between items-center px-4 py-2 text-sm cursor-pointer text-gray-500 hover:text-green-500"
+                            onClick={() => navigate(`/post/edit/${data.id}`)}
+                          >
+                            Edit
+                            <BiSolidEditAlt className=" cursor-pointer text-lg transition-all" />
+                          </div>
+                        </Menu.Item>
+                        <Menu.Item>
+                          <div
+                            className="flex justify-between items-center px-4 py-2 text-sm cursor-pointer text-gray-500 hover:text-red-500"
+                            onClick={() => deleteModalFn(data.id)}
+                          >
+                            Delete
+                            <AiFillDelete className=" cursor-pointer text-lg transition-all" />
+                          </div>
+                        </Menu.Item>
+                      </div>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
               )}
             </div>
             <div className="flex justify-between items-center gap-3">
@@ -229,6 +267,10 @@ const PostCard = ({ data, handleCommentClick, classes }) => {
             Share
           </p>
         </div>
+
+        {/*  */}
+
+        {/*  */}
 
         <DeleteConfirmation
           type="Post"
